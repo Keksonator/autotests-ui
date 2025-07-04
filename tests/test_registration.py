cf7097b33c68faa5696cc96f0b04c8e0ebc7a531
Rodion <Keksonatorver2@gmail.com>
2025-07-04 08:30:31 +0300

    Знакомство с PageObject. Практика работы с Page Object.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,3 +1,5 @@
+import time
+
 from playwright.sync_api import sync_playwright, expect
 import pytest
 from pages.registration_page import RegistrationPage
@@ -6,14 +8,8 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-@pytest.mark.parametrize("email, username, password", [
-    ("user.name@gmail.com", "Username", "password"),
-]
-                         )
-def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage, email: str,
-                                 username: str, password: str):
+def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage):
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.fill_registration_form(email="user.name@gmail.com", username="username", password="password")
     registration_page.click_registration_button()
-    dashboard_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-    dashboard_page.check_dashboard_title()
+    dashboard_page.check_visible_dashboard_title()
